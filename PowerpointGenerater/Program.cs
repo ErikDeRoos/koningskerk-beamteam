25e18246cb17fafa5213a8fe66721b905ef40796
coordinator <xorgye@gmail.com>
2015-10-14 18:16:13 +0000

    Relatief maken paden, begin gemaakt met verbeteren verdelen liedteksten over slides

--- PowerpointGenerater/Program.cs
+++ PowerpointGenerater/Program.cs
@@ -5,6 +5,16 @@ using System.Windows.Forms;
 
 namespace PowerpointGenerater
 {
+    
+    //TODO: de volgorde in de liturgie bijbellezingen op de juiste plek;
+    //TODO: de nummering van de liederen (1/2, 2/2 etc.)
+    //TODO: zwarte dia tonen
+    //TODO: “1e collecte” wijzigen in “collecte”
+    //TODO: het woordje “liturgie” weg boven de het liturgiebord
+    //TODO: de lay-out van het programma
+    //TODO: het mogelijk maken om in het programma te kunnen werken met bijvoorbeeld “rechtmuisknop, plakken”
+
+
     static class Program
     {
         /// <summary>
---
+++ PowerpointGenerater/StandaardTeksten.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PowerpointGenerater
+{
+    public class StandaardTeksten
+    {
+        public string Volgende { get; set; }
+        public string Voorganger { get; set;}
+        public string Collecte1 { get; set;}
+        public string Collecte2 { get; set;}
+        public string Collecte { get; set;}
+        public string Lezen { get; set;}
+        public string Tekst { get; set;}
+        public string Liturgie { get; set;}
+    }
+}
