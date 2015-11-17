0b349ad3fde8a4560682ea2bfeb46d5d5e0b2ec8
coordinator <xorgye@gmail.com>
2015-11-17 14:40:00 +0000

    Erik de Roos, betere foutmeldingen schrijven

---
+++ PowerpointGenerater/FoutmeldingSchrijver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PowerpointGenerater {
+  class FoutmeldingSchrijver {
+    public static void Log(Exception melding) {
+      Log(melding.ToString());
+    }
+    public static void Log(String melding) {
+      using (var sw = new StreamWriter("ppgenerator.log", true)) {
+        sw.WriteLine(String.Format("[{0}] Foutmelding door [{1}] op systeem [{2}]", DateTime.Now, Environment.UserName, Environment.MachineName));
+        sw.WriteLine(melding);
+      }
+    }
+  }
+}
