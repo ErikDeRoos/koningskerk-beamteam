4ca70c91842877a77a6a326df51e6c9380a4db61
Erik de Roos <erik.de.roos@topicus.nl>
2016-02-18 16:51:44 +0100

    Poco objecten verder uitgewerkt
    --HG--
    branch : remote_generator

---
+++ ConnectTools/Berichten/LiturgieRegelContent.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ConnectTools.Berichten
+{
+    public class LiturgieRegelContent
+    {
+        /// <summary>
+        /// Nummering. Bij geen nummer worden er geen nummers getoond
+        /// </summary>
+        public int? Nummer { get; set; }
+        /// <summary>
+        /// Type inhoud.
+        /// </summary>
+        public InhoudType InhoudType { get; set; }
+        /// <summary>
+        /// De inhoud. De actuele tekst bij tekst, een link bij ppt.
+        /// </summary>
+        public string Inhoud { get; set; }
+    }
+}
