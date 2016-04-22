3e428906fefed18a44fc218d26ec13ba04d91be9
Erik de Roos <xorgye@gmail.com>
2016-04-22 14:37:53 +0200

    Op instellingen scherm een template voor bijbeltekst toegevoegd en een regel per bijbeltekst slide toegevoegd, Layout lijnen van alle elementen gecontroleerd.
    --HG--
    branch : feature/22-bijbelteksten

--- PowerpointGenerator/Screens/Instellingenform.Designer.cs
+++ PowerpointGenerator/Screens/Instellingenform.Designer.cs
@@ -69,6 +69,11 @@
             this.lblVolgende = new System.Windows.Forms.Label();
             this.tbVoorganger = new System.Windows.Forms.TextBox();
             this.lblVoorganger = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.pnlButtons.SuspendLayout();
             this.tabPanel.SuspendLayout();
             this.tabAlgemeen.SuspendLayout();
@@ -126,6 +131,11 @@
             // 
             // tabAlgemeen
             // 
+            this.tabAlgemeen.Controls.Add(this.textBox7);
+            this.tabAlgemeen.Controls.Add(this.label8);
+            this.tabAlgemeen.Controls.Add(this.textBox6);
+            this.tabAlgemeen.Controls.Add(this.button7);
+            this.tabAlgemeen.Controls.Add(this.label9);
             this.tabAlgemeen.Controls.Add(this.button6);
             this.tabAlgemeen.Controls.Add(this.textBox5);
             this.tabAlgemeen.Controls.Add(this.label7);
@@ -152,7 +162,7 @@
             // button6
             // 
             this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button6.Location = new System.Drawing.Point(801, 118);
+            this.button6.Location = new System.Drawing.Point(802, 118);
             this.button6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(57, 34);
@@ -165,7 +175,7 @@
             // 
             this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox5.Location = new System.Drawing.Point(13, 119);
+            this.textBox5.Location = new System.Drawing.Point(14, 119);
             this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(790, 26);
@@ -175,7 +185,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(8, 95);
+            this.label7.Location = new System.Drawing.Point(10, 94);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(105, 20);
@@ -184,7 +194,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(150, 320);
+            this.textBox4.Location = new System.Drawing.Point(176, 400);
             this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(194, 26);
@@ -193,12 +203,12 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 325);
+            this.label4.Location = new System.Drawing.Point(10, 403);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(130, 20);
+            this.label4.Size = new System.Drawing.Size(158, 20);
             this.label4.TabIndex = 23;
-            this.label4.Text = "Regels per slide: ";
+            this.label4.Text = "Regels per lied slide: ";
             // 
             // textBox3
             // 
@@ -226,7 +236,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 19);
+            this.label3.Location = new System.Drawing.Point(10, 17);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(137, 20);
@@ -259,7 +269,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 171);
+            this.label2.Location = new System.Drawing.Point(9, 170);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(136, 20);
@@ -292,12 +302,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 244);
+            this.label1.Location = new System.Drawing.Point(10, 244);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.Size = new System.Drawing.Size(117, 20);
             this.label1.TabIndex = 14;
-            this.label1.Text = "Liederen Template: ";
+            this.label1.Text = "Lied Template: ";
             // 
             // tabStdTeksten
             // 
@@ -530,6 +540,56 @@
             this.lblVoorganger.TabIndex = 0;
             this.lblVoorganger.Text = "Voorganger";
             // 
+            // textBox6
+            // 
+            this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox6.Location = new System.Drawing.Point(14, 346);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(790, 26);
+            this.textBox6.TabIndex = 31;
+            this.textBox6.Text = "Path";
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Location = new System.Drawing.Point(802, 345);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(57, 34);
+            this.button7.TabIndex = 30;
+            this.button7.Text = "...";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(10, 321);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(160, 20);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "Bijbeltekst Template: ";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(665, 400);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(194, 26);
+            this.textBox7.TabIndex = 33;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(452, 403);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(205, 20);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "Regels per bijbeltekst slide: ";
+            // 
             // Instellingenform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -593,5 +653,10 @@
         private System.Windows.Forms.Label label7;
         public System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button6;
+        public System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label9;
+        public System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
     }
 }
