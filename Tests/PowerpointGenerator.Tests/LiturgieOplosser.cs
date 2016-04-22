eb405905e6601fc07a222fdd8a12f95d403c1a06
Erik de Roos <xorgye@gmail.com>
2016-04-22 14:24:57 +0200

    Tests voor zoeken in specifiek bijbeltekst database
    --HG--
    branch : feature/22-bijbelteksten

--- Tests/PowerpointGenerator.Tests/LiturgieOplosser.cs
+++ Tests/PowerpointGenerator.Tests/LiturgieOplosser.cs
@@ -120,4 +120,61 @@ namespace Generator.Tests
             return database;
         }
     }
+
+    public class LiturgieOplosser_Bijbeltekst
+    {
+        private static string DefaultEmptyName = "!leeg";
+
+        [TestCase("Johannes", "3", new [] { "1" ,"2", "3"})]
+        public void LosOp_NormaalItem_ZoekInDatabase(string onderdeel, string fragment, IEnumerable<string> fragmentVerzen)
+        {
+            var liturgieItem = FakeBijbeltekstInterpretatie(onderdeel, fragment, fragmentVerzen);
+            var database = FakeDatabase(onderdeel, fragment);
+            var sut = (new Generator.LiturgieOplosser.LiturgieOplosser(database, DefaultEmptyName)) as ILiturgieLosOp;
+
+            var oplossing = sut.LosOp(liturgieItem);
+
+            A.CallTo(() => database.ZoekOnderdeel(VerwerkingType.bijbeltekst, liturgieItem.Benaming, liturgieItem.PerDeelVersen.First().Deel, liturgieItem.PerDeelVersen.First().Verzen)).MustHaveHappened();
+        }
+
+        [TestCase("Johannes", "3", new[] { "1", "2", "3" })]
+        public void LosOp_NormaalItem_Gevonden(string onderdeel, string fragment, IEnumerable<string> fragmentVerzen)
+        {
+            var liturgieItem = FakeBijbeltekstInterpretatie(onderdeel, fragment, fragmentVerzen);
+            var database = FakeDatabase(onderdeel, fragment);
+            var sut = (new Generator.LiturgieOplosser.LiturgieOplosser(database, DefaultEmptyName)) as ILiturgieLosOp;
+
+            var oplossing = sut.LosOp(liturgieItem);
+
+            Assert.That(oplossing.Resultaat, Is.EqualTo(LiturgieOplossingResultaat.Opgelost));
+        }
+
+        private static ILiturgieInterpretatieBijbeltekst FakeBijbeltekstInterpretatie(string onderdeel, string fragment, IEnumerable<string> verzen)
+        {
+            var liturgieItem = A.Fake<ILiturgieInterpretatieBijbeltekst>();
+            A.CallTo(() => liturgieItem.Benaming).Returns(onderdeel);
+            A.CallTo(() => liturgieItem.Deel).Returns(fragment);
+            var liturgieItemDeel = A.Fake<ILiturgieInterpretatieBijbeltekstDeel>();
+            A.CallTo(() => liturgieItemDeel.Deel).Returns(fragment);
+            A.CallTo(() => liturgieItemDeel.Verzen).Returns(verzen.ToList());
+            A.CallTo(() => liturgieItem.PerDeelVersen).Returns(new[] { liturgieItemDeel });
+            return liturgieItem;
+        }
+
+        private static ILiturgieDatabase.ILiturgieDatabase FakeDatabase(string onderdeel, string fragment, string display = null)
+        {
+            var zoekresultaat = A.Fake<IZoekresultaat>();
+            A.CallTo(() => zoekresultaat.OnderdeelNaam).Returns(onderdeel);
+            A.CallTo(() => zoekresultaat.FragmentNaam).Returns(fragment);
+            if (display != null)
+                A.CallTo(() => zoekresultaat.OnderdeelDisplayNaam).Returns(display);
+            var database = A.Fake<ILiturgieDatabase.ILiturgieDatabase>();
+            A.CallTo(database)
+                .Where(d => d.Method.Name == "ZoekOnderdeel")
+                .WithReturnType<IZoekresultaat>()
+                .WithAnyArguments()
+                .Returns(zoekresultaat);
+            return database;
+        }
+    }
 }
