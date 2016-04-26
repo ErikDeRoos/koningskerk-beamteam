4fbfbec31e6a8c5993837872f5968fe450665f2c
Erik de Roos <xorgye@gmail.com>
2016-04-26 11:59:43 +0200

    Tool voor opknippen zin in woorden.
    --HG--
    branch : feature/22-bijbelteksten

--- Tools/SplitRegels.cs
+++ Tools/SplitRegels.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright 2016 door Erik de Roos
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Tools
 {
@@ -10,6 +12,14 @@ namespace Tools
             if (list == null)
                 return new string[0];
             return list.Replace("\r", "").Split(new[] { "\n" }, StringSplitOptions.None);
+        }
+
+        public static IEnumerable<string> KnipInWoorden(string regel)
+        {
+            foreach (Match match in Regex.Matches(regel, "([^ .,!?-]+)([ .,!?-]+)?", RegexOptions.Compiled))
+            {
+                yield return match.Value;
+            }
         }
     }
 }
