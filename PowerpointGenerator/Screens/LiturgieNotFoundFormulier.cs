81c0c7d9e82be217e37745298299a83e699a80f9
ErikDeRoos <xorgye@gmail.com>
2017-04-14 21:11:12 +0200

    Betere foutmeldingen als er fouten in de liturgie zitten.

--- PowerpointGenerator/Screens/LiturgieNotFoundFormulier.cs
+++ PowerpointGenerator/Screens/LiturgieNotFoundFormulier.cs
@@ -12,8 +12,30 @@ namespace PowerpointGenerator
         {
             InitializeComponent();
             textBox1.Lines = fouten
-                .Select(l => $"{l.VanInterpretatie.Benaming} {l.VanInterpretatie.Deel}: {l.Resultaat}")
+                .Select(l => $"{l.VanInterpretatie.Benaming} {l.VanInterpretatie.Deel}: {KrijgHelpendeTekstBijLiturgieFout(l.Resultaat, l.VanInterpretatie)}")
                 .ToArray();
+        }
+
+        public static string KrijgHelpendeTekstBijLiturgieFout(LiturgieOplossingResultaat resultaatFout, ILiturgieInterpretatie vanInterpretatie)
+        {
+            switch (resultaatFout)
+            {
+                case LiturgieOplossingResultaat.DatabaseFout:
+                    return $"De tekst achter 'als' is niet correct.";
+                case LiturgieOplossingResultaat.SetFout:
+                    return $"'{vanInterpretatie.Benaming}' is niet bekend lied, bijbelboek of slide.";
+                case LiturgieOplossingResultaat.SubSetFout:
+                    return string.IsNullOrWhiteSpace(vanInterpretatie.Deel) ? "Je moet een hoofdstuk of lied opgeven." : $"'{vanInterpretatie.Deel}' is niet te vinden in {vanInterpretatie.Benaming}.";
+                case LiturgieOplossingResultaat.VersFout:
+                    return $"Niet alle verzen konden gevonden worden in de database.";
+                case LiturgieOplossingResultaat.VersOnderverdelingMismatch:
+                    return $"Bij '{vanInterpretatie.Deel} {vanInterpretatie.Deel}' kan je helaas geen specifieke verzen opgeven.";
+                case LiturgieOplossingResultaat.VersOnleesbaar:
+                    return $"Een van de verzen geeft aan dat er geen inhoud aan hem gekoppeld is in de database. Kan je niet echt iets mee, maar dat gaat nu dus wel fout.";
+                case LiturgieOplossingResultaat.Onbekend:
+                    return "Er is iets goed mis, ik kan je niet verder helpen.";
+            }
+            return string.Empty;
         }
     }
 }
