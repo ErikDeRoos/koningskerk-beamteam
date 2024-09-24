8bd186b9e674b23132ecdbaeb4e75715ffdbd5a6
Erik de Roos <17176362+ErikDeRoos@users.noreply.github.com>
2024-09-24 16:04:02 +0200

    Fix op de library upgrade die niet op het gehele project uitgevoerd bleek te zijn, nu wel en nu bouwen alle componenten niet alleen de unittests.

--- PowerpointGenerator/Properties/Resources.Designer.cs
+++ PowerpointGenerator/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace PowerpointGenerator.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -88,7 +88,7 @@ namespace PowerpointGenerator.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Powerpoint bestand (*.ppt)|*.ppt|Powerpoint bestanden (*.pptx)|*.pptx.
+        ///   Looks up a localized string similar to Powerpoint bestanden (*.pptx)|*.pptx|Powerpoint bestand (*.ppt)|*.ppt.
         /// </summary>
         internal static string Form1_Opslaan_pp_filter {
             get {
